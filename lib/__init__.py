7e12c9568a53df2814162316a7fa17ce8ab9852e
qalicha-dev28 <qalichanajma44@gmail.com>
2025-08-29 14:16:29 +0300

    Modified init.py

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,3 +1,6 @@
-from .models import Base, engine, Session, Category, Habit, Completion
-
-__all__ = ['Base', 'engine', 'Session', 'Category', 'Habit', 'Completion']
+from models import session, Category, Habit, Completion
+from crud import (
+    create_category, get_all_categories,
+    create_habit, get_all_habits,
+    log_completion, get_completions, get_habit_streak
+)
